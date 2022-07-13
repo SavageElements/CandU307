5afb3e1f33b99f535b90ab9150bb9269669186c2
J Rockwell Seebach <jrs@ironpinephoto.com>
2022-07-13 00:52:58 -0700

    Add project files.

---
+++ CandU307/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CandU307
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please enter a number");
+            int xHours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In " + xHours + " hours, it will be:");
+
+            DateTime now = DateTime.Now;
+            DateTime D2 = now.AddHours(+xHours);
+            Console.WriteLine(D2);
+        }
+    }
+}
